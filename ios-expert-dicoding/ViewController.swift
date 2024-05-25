f0a36635e6746840d7a099e591f96424e098c5e3
Sad Keenanda Adityo <t-sad.adityo@traveloka.com>
2024-05-25 16:25:31 +0700

    create project and remove storyboard

---
+++ ios-expert-dicoding/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ios-expert-dicoding
+//
+//  Created by Sad Keenanda Adityo on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.green
+    }
+    
+    
+
+}
+
